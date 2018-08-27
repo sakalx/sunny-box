511280668f360d0ab03558694cf8cc8b99f338cd
sakal <sakals@mail.ua>
2018-08-27 08:00:01 -0400

    fix: get Data
    Signed-off-by: sakal <sakals@mail.ua>

--- hard-code.js
+++ hard-code.js
@@ -17,6 +17,49 @@ firebase.initializeApp({
 const firestore = firebase.firestore();
 firestore.settings({timestampsInSnapshots: true});
 
+const TOKEN = '2227bc9dbeeea2063129a72a3d'; // 146861067c97180b6e3fc79f34 - 2227bc9dbeeea2063129a72a3d
+
+function data(from, to) {
+  const chunk = geo.objects.countries1.geometries.slice(from, to);
+  const times = to - from;
+  const countriesList = chunk.map(({properties}) => properties['Alpha-2']);
+
+  doRequest(times, countriesList)
+}
+
+
+function doRequest(times, countriesList) {
+  if (times < 0) return;
+
+  setTimeout(() => {
+    const countryCode = countriesList[times];
+    console.log(`Do request # ${times} country ${countryCode}`);
+
+    countryCode && getData(countryCode, 5);
+    doRequest(times - 1, countriesList);
+  }, 7000);
+}
+
+
+function getData(countryCode, pages) {
+  const country = geo.objects.countries1.geometries
+    .find(({properties}) => properties['Alpha-2'] === countryCode);
+
+  if (!country) {
+    console.log('hmm... country name not found');
+    return
+  }
+
+  const url = page => `http://api.dirble.com/v2/countries/${countryCode.toLowerCase()}/stations/?token=${TOKEN}&per_page=30&page=${page}`;
+  const api = Array.from(Array(pages)).map((x, i) => axios.get(url(i + 1)));
+
+  axios.all(api)
+    .then(axios.spread((...responses) => merge(responses)))
+    .then(data => createOurData(data))
+    .then(data => sendToFirestore(country.properties.name, data))
+    .catch(error => console.error('api', error));
+}
+
 
 function merge(responses) {
   const merged = [];
@@ -24,6 +67,7 @@ function merge(responses) {
   responses.forEach(({data}) => merged.push(...data));
   return merged;
 }
+
 
 function createOurData(stations) {
 
@@ -46,30 +90,11 @@ function createOurData(stations) {
     }, {});
 }
 
+
 function sendToFirestore(country, data) {
   firestore.doc(`stations/${country}`).set(data)
-    .then(() => console.log('should be good'))
+    .then(() => console.log('should be good 🎈 😈'))
     .catch(error => console.error('Firebase:', error));
 }
 
-function getData(countryCode, pages) {
-  const country = geo.objects.countries1.geometries
-    .find(({properties}) => properties['Alpha-2'] === countryCode);
-
-  if (!country) {
-    console.log('hmm... country name not found');
-    return
-  }
-
-  const url = page => `http://api.dirble.com/v2/countries/${countryCode.toLowerCase()}/stations/?token=146861067c97180b6e3fc79f34&per_page=2&page=${page}`;
-  const api = Array.from(Array(pages)).map((x, i) => axios.get(url(i + 1)));
-
-  axios.all(api)
-    .then(axios.spread((...responses) => merge(responses)))
-    .then(stations => createOurData(stations))
-    .then(data => sendToFirestore(country.properties.name, data))
-    .catch(error => console.error('api', error));
-}
-
-
-getData('RU', 1);
+data(160, 180); // max 177
